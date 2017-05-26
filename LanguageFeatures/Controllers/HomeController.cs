c5017365ae601aeaf98305f1358cdfe5205d426b
Bill Adams <billadams1977@gmail.com>
2017-05-26 12:54:54 -0500

    initial commit

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to shaw an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            // Create a new Product object
+            Product myProduct = new Product();
+
+            // Set the property value
+            myProduct.ProductID = 100;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "A boat for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersports";
+
+            // Get the property
+            //string productName = myProduct.Name;
+
+            // Generate the view
+            return View("Result",
+                (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                { "apple", 10 }, { "orange", 20 }, { "plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult UseExtension()
+        {
+            // Create and populate ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product { Name = "Kayak", Price = 275M },
+                    new Product { Name = "Lifejacket", Price = 48.95M },
+                    new Product { Name = "Soccer ball", Price = 19.50M },
+                    new Product { Name = "Corner flag", Price = 34.95M }
+                }
+            };
+
+            // Get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result",
+                (object)String.Format("Total: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product { Name = "Kayak", Price = 275M },
+                    new Product { Name = "Lifejacket", Price = 48.95M },
+                    new Product { Name = "Soccer ball", Price = 19.50M },
+                    new Product { Name = "Corner flag", Price = 34.95M }
+                }
+            };
+
+            // Create and populate an array of Product objects
+            Product[] productArray =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M }
+            };
+
+            // Get the total value of the products in the cart
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = products.TotalPrices();
+
+            return View("Result",
+                (object)String.Format("Cart Total: {0}, Array total: {1}",
+                    cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+                    new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+                    new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                    new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in products.FilterByCategory("Soccer"))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
+    }
+}
